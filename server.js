42c51ea6e2fb2eb15ab11a615a3fc9185cf3a4d2
Vu Nguyen <hggaming91@gmail.com>
2022-07-08 21:06:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express =  require("express");
-const axios = require("axios");
+import express from "express";
+import axios from "axios";
 const app = express();
 
 const PORT = process.env.PORT || 3002;
